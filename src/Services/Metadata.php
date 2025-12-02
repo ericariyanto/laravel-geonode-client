3611d903dd9b33081e6042017c095ecbcaf89f64
Eric Ariyanto <mail@ericariyanto.com>
2025-12-02 13:35:57 +0700

    Fix Bug Update Metadata

--- src/Services/Metadata.php
+++ src/Services/Metadata.php
@@ -13,24 +13,51 @@ class Metadata
         $this->http = $http;
     }
 
-    public function updateTags(int $id, array $tags)
+    /**
+     * Update multiple basic metadata fields at once.
+     */
+    public function update(int $id, array $data)
     {
-        return $this->http->patch("/api/v2/datasets/{$id}/", [
-            'keywords' => $tags,
-        ]);
+        return $this->http->patch("/api/v2/datasets/{$id}/", $data);
+    }
+
+    public function updateTitle(int $id, string $title)
+    {
+        return $this->update($id, ['title' => $title]);
+    }
+
+    public function updateAbstract(int $id, string $abstract)
+    {
+        return $this->update($id, ['abstract' => $abstract]);
+    }
+
+    public function updatePurpose(int $id, string $purpose)
+    {
+        return $this->update($id, ['purpose' => $purpose]);
+    }
+
+    public function updateCategory(int $id, string $category)
+    {
+        return $this->update($id, ['category' => $category]);
+    }
+
+    public function updateKeywords(int $id, array $keywords)
+    {
+        return $this->update($id, ['keywords' => $keywords]);
     }
 
     public function updateRegions(int $id, array $regions)
     {
-        return $this->http->patch("/api/v2/datasets/{$id}/", [
-            'regions' => $regions,
-        ]);
+        return $this->update($id, ['regions' => $regions]);
     }
 
-    public function updateGroup(int $id, string $group)
+    public function updateGroup(int $id, ?string $group)
     {
-        return $this->http->patch("/api/v2/datasets/{$id}/", [
-            'group' => $group,
-        ]);
+        return $this->update($id, ['group' => $group]);
+    }
+
+    public function updateLicense(int $id, array $license)
+    {
+        return $this->update($id, ['license' => $license]);
     }
 }
